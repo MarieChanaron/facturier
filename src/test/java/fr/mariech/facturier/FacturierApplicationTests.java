4f46fe9e429ecbd35fb052b5a511fa5a97a74b66
Marie Chanaron <chanaronmarie@gmail.com>
2023-05-17 10:12:11 +0200

    Initialize a new project - Include packages - application.properties with the right settings

---
+++ src/test/java/fr/mariech/facturier/FacturierApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.mariech.facturier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
